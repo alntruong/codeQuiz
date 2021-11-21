d9852fad4ead854afdb7042279234d4cec68ad92
Alan Truong <alntruong@ucdavis.edu>
2021-11-21 15:36:15 -0800

    commit

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+ {
+  title: "Inside which HTML element do we put the JavaScript?",
+  choices: ["<js>", "<scripting>", "<javascript>", "<script>"],
+  answer: "<script>"
+ },
+ {
+  title: "What is the correct JavaScript syntax to change the following content of the HTML element: <p id='demo'>This is a demonstration.</p>",
+  choices: ["document.getElement('p').innerHTML='Hello World!';", "#demo.innerHTML='Hello World!';", "document.getElementByName('p').innerHTML='Hello World!';", "document.getElementById('p').innerHTML='Hello World!';"],
+  answer: "document.getElementById('p').innerHTML='Hello World!';"
+ },
+ {
+  title: "Where is the correct place to inset a JavaScript?",
+  choices: [
+   "The <body> section",
+   "Both the <head> section and the <body> section",
+   "The <head> section",
+   "None of the above"
+  ],
+  answer: "Both the <head> section and the <body> section"
+ },
+ {
+  title:
+   "How do you write 'Hello World' in an alert box?",
+  choices: ["msg('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');", "alert('Hello World');"],
+  answer: "alert('Hello World');"
+ },
+ {
+  title:
+   "A very useful tool used during development and debugging for printing content to the debugger is:",
+  choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+  answer: "console.log"
+ }
+];
